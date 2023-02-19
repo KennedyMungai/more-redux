989edb98d001073e27efd04a67268f6651476973
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-19 15:56:40 +0300

    Got rid of the store import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import store from './store';
 
 function App()
 {
