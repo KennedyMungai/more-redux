7712701968d8933d8606876676327365dca826f1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-19 15:47:30 +0300

    Created the cartSlice skeleton

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,2 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const cartSlice = createSlice({
+    name: 'Cart',
+    initialState: {},
+    reducers: {
+        addToCart()
+        {
+
+        }
+    }
+})
+
+export const cartActions = cartSlice.actions
+
+export default cartSlice
