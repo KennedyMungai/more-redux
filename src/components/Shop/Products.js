231816dd2782becedf262aff4ec72d22ea1a9eb8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-19 16:17:38 +0300

    Got rid of the braces

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -24,14 +24,13 @@ const Products = (props) =>
       <ul>
         {
           DUMMY_PRODUCTS.map((product) =>
-          {
             <ProductItem
               key={product.id}
               title={product.title}
               price={product.price}
               description={product.description}
             />
-          })
+          )
         }
 
       </ul>
