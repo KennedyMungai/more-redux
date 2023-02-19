64042cd445df509444c25e80bcb4a7ebffc701f0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-19 12:34:20 +0300

    Specified the uiSlice reducer

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import uiSlice from "./ui-slice";
 
 const store = configureStore({
-    reducer: { ui: uiSlice }
+    reducer: { ui: uiSlice.reducer }
 })
 
 export default store
