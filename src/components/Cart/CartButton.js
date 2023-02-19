31a836e4869046145e589d29b9d99fad10e6837d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-19 15:33:01 +0300

    Added an import for the useSelector hook

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
