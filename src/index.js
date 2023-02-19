d5b2633bc6ff830201f18dfb14ac4c47010fc114
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-19 12:35:25 +0300

    Added an import for teh provider

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(<App />, document.getElementById('root'));
