1a485cfea0a6780bd52e938deef7927a817f4c62
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-19 16:51:53 +0300

    Used the proper order and quantity of items in the object

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,9 +11,16 @@ const Cart = (props) =>
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cartItems.map((item) => (
+          <CartItem
+            item={{
+              title: item.name,
+              quantity: item.quantity,
+              price: item.price,
+              total: item.totalPrice,
+            }}
+          />
+        ))}
       </ul>
     </Card>
   );
