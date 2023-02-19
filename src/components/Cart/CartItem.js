7fd4b736139db7e74e33b6f04fe9fdd3215abb5c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-19 16:58:33 +0300

    Added an import for the useDispatch hook

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,6 +1,8 @@
+import { useDispatch } from 'react-redux';
 import classes from './CartItem.module.css';
 
-const CartItem = (props) => {
+const CartItem = (props) =>
+{
   const { title, quantity, total, price } = props.item;
 
   return (
