a4eeb591427fd811455736198f63bc0e2bee3124
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-19 12:26:50 +0300

    Created the ui slice

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,1 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+
+const ui = createSlice({
+    name: 'ui',
+    initialState: { cartIsVisible: false },
+    reducers: {
+        toggle(state)
+        {
+            state.cartIsVisible = !state.cartIsVisible
+        }
+    }
+})
