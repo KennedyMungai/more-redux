1336c852cbd595bffcd6a0782eff295ac522f0d0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-20 14:35:58 +0300

    Defined the fetchCartData function

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -43,3 +43,36 @@ export const sendCartData = (cart) =>
         }
     }
 }
+
+export const fetchCartData = () => 
+{
+    return async (dispatch) => 
+    {
+        const fetchData = async () =>
+        {
+            const response = await fetch('https://simple-react-backend-default-rtdb.firebaseio.com/cart.json')
+
+            if (!response.ok)
+            {
+                throw new Error('Could not fetch cart data')
+            }
+
+            const data = await response.json()
+
+            return data
+        }
+
+        try
+        {
+            const cartData = await fetchData()
+        }
+        catch (error)
+        {
+            dispatch(uiActions.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Fetching cart data failed'
+            }))
+        }
+    }
+}
