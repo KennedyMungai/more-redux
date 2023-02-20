27869be71663db8d43dbf2434d67ca78b69cab49
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-20 14:06:11 +0300

    Moved some of the code to the cartSlice

--- src/App.js
+++ src/App.js
@@ -20,23 +20,6 @@ function App()
   {
     const sendCartData = async () => 
     {
-      dispatch(uiActions.showNotification(
-        {
-          status: 'pending',
-          title: 'sending...',
-          message: 'Sending cart data'
-        }))
-
-      const response = await fetch('https://simple-react-backend-default-rtdb.firebaseio.com/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      })
-
-      if (!response.ok)
-      {
-        throw new Error("Sending Cart Data failed")
-      }
-
       dispatch(uiActions.showNotification({
         status: 'success',
         title: 'Success!',
