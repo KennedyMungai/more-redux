06f1f52675f80296de96dab740b116f06023d5fb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-20 14:15:52 +0300

    Got rid of the uiActions slice import

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/ui-slice';
 
 let isInitial = true
 
