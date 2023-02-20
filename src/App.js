2a0011d96dc9dd2373f6ce4de69efaaec714bcc7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-20 14:42:03 +0300

    Created a new useEffect block to dispatch the fetchCartData whoch is only meant to run at app startup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { sendCartData } from './store/cart-actions';
+import { fetchCartData, sendCartData } from './store/cart-actions';
 
 let isInitial = true
 
@@ -15,6 +15,12 @@ function App()
   const isCartVisible = useSelector(state => state.ui.cartIsVisible)
   const cart = useSelector((state) => state.cart)
   const notification = useSelector((state) => state.ui.notification)
+
+  useEffect(() =>
+  {
+    dispatch(fetchCartData())
+  }, [dispatch])
+
 
   useEffect(() =>
   {
