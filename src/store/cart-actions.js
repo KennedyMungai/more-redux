fbf0c23ef050ad74fc61f5f67672b60b2a63d63f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-20 14:27:57 +0300

    Moved tyhe sendCartData function to the cart actions js file

---
+++ src/store/cart-actions.js
@@ -0,0 +1,45 @@
+import { uiActions } from "./ui-slice"
+
+export const sendCartData = (cart) => 
+{
+    return async (dispatch) => 
+    {
+        dispatch(uiActions.showNotification({
+            status: 'pending',
+            title: 'Sending',
+            message: 'Sending cart data!'
+        }))
+
+        const sendRequest = async () =>
+        {
+            const response = await fetch('https://simple-react-backend-default-rtdb.firebaseio.com/cart.json', {
+                method: 'PUT',
+                body: JSON.stringify(cart),
+            })
+
+            if (!response.ok)
+            {
+                throw new Error("Sending Cart Data failed")
+            }
+        }
+
+        try
+        {
+            await sendRequest()
+
+            dispatch(uiActions.showNotification({
+                status: 'success',
+                title: 'Success!',
+                message: 'Sending cart data successfully!'
+            }))
+        }
+        catch (error)
+        {
+            dispatch(uiActions.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Sending cart data failed'
+            }))
+        }
+    }
+}
