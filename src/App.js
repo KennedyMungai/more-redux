1a1adf2eee77d39890eb924934292b0019096c38
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-20 13:45:18 +0300

    Imported the notification component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
 import { uiActions } from './store/ui-slice';
 
 function App()
