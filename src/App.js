f0ef7f797e788eff8ef18f9c23f919aef1c328d6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-20 13:01:28 +0300

    Dispatched the showNotifications function

--- src/App.js
+++ src/App.js
@@ -16,6 +16,13 @@ function App()
   {
     const sendCartData = async () => 
     {
+      dispatch(uiActions.showNotification(
+        {
+          status: 'pending',
+          title: 'sending...',
+          message: 'Sending cart data'
+        }))
+
       const response = await fetch('https://simple-react-backend-default-rtdb.firebaseio.com/cart.json', {
         method: 'PUT',
         body: JSON.stringify(cart),
