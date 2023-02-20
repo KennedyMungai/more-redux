4d4e728e41525b555467b6f51f321120bd566441
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-20 14:51:56 +0300

    Specified the properties being send to firebase

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -15,7 +15,12 @@ export const sendCartData = (cart) =>
         {
             const response = await fetch('https://simple-react-backend-default-rtdb.firebaseio.com/cart.json', {
                 method: 'PUT',
-                body: JSON.stringify(cart),
+                body: JSON.stringify(
+                    {
+                        items: cart.items,
+                        totalQuantity: cart.totalQuantity
+                    }
+                ),
             })
 
             if (!response.ok)
